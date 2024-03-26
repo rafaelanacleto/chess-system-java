4c4d1f60eae21b326e8441897b691edfd7fae74d
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-03-25 22:02:26 -0300

    Finalizado a classe position.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World KJA");
+
+		Position pos = new Position(10, 20);
+		
+		System.out.println(pos.toString());
 
 	}
 
